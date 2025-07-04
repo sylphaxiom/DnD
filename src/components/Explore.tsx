eb696591c07a3912e4ae01e10c39d5d1eb9c4a3d
Jacob Pell <pelljacoba@gmail.com>
2025-07-03 22:21:55 -0400

    Adding untracked new files.

---
+++ src/components/Explore.tsx
@@ -0,0 +1,42 @@
+//import React from 'react'
+
+const Explore = () => {
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <h2 className="col-md-2">What is Kothis?</h2>
+        <div className="col-md-10">
+          <p>
+            Kothis is a homebrewed setting, originally created for Dungeons &
+            Dragons (5e). During the course of 2 campaigns there has been a lot
+            of characters, lore, events, and even items that have been created.
+            They all feel a part of this world and I want to include them all in
+            a centralized place. This site will serve as the interactive
+            connnection to the world of Kothis. Here you will be able to view
+            your upcoming games set in this world, communicate with your party
+            or GM, and even maintain your characters that live in this world!
+          </p>
+          <br />
+          <p>
+            The amount of information you have available to you without
+            requesting access is limited to the basic information about the
+            world and D&D in general, but you can always request further access
+            by joining a game or hosting one of your own! This world is ever
+            expanding as we continue to play games within the setting. We hope
+            to continue to grow the depth within the world with variety and
+            interactions. It is because of this desire for depth (and an
+            abundance of safety!) that we promote a safe environment in all of
+            our games played within this setting. We have guidelines set up for
+            anyone who wishes to join or create a game within the world that we
+            ask that you follow. These are simple safety guidelines which amount
+            to a good ole' fashioned "Don't be a d**k" rule of thumb. Respect
+            your players and your Game Master and I'm sure everyone will enjoy
+            their time in the world of Kothis!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Explore;
