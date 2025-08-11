1e000e2bac70c03e8ff595953530bf33f29ebc47
Jacob Pell <pelljacoba@gmail.com>
2025-08-11 12:27:59 -0400

    Set of tests for each page. It only checks the url after clicking a nav link to validate each page.

--- tests/app.spec.ts
+++ tests/app.spec.ts
@@ -1,12 +1,88 @@
 import { test, expect } from '@playwright/test';
 
-test.afterEach(async ({ page}) => {
-  await page.close();
-});
-
-test('has title', async ({ page }) => {
+test.beforeEach(async ({ page }) => {
+  
+  // Using '/' for testing.
   await page.goto('/');
 
-  // Expect a title "to contain" a substring.
+  // Expect a title.
   await expect(page).toHaveTitle('World of Kothis');
+
+});
+
+test.afterEach(async ({ page}) => {
+
+  // Close out the page
+  await page.close();
+
+});
+
+test('is home page', async ({ page }) => {
+  
+  // Click nav link 
+  await page.getByRole('link', { name: 'Home', exact: true }).click();
+
+  // Expect url to become the set page. Additional tests to follow.
+  await expect(page).toHaveURL('#home');
+
+});
+
+test('is characters page', async ({ page }) => {
+  
+  // Click nav link 
+  await page.getByRole('link', { name: 'Characters', exact: true }).click();
+  
+  // Expect url to become the set page. Additional tests to follow.
+  await expect(page).toHaveURL('#characters');
+
+});
+
+test('is campaigns page', async ({ page }) => {
+  
+  // Click nav link 
+  await page.getByRole('link', { name: 'Campaigns', exact: true }).click();
+  
+  // Expect url to become the set page. Additional tests to follow.
+  await expect(page).toHaveURL('#campaigns');
+
+});
+
+test('is notebook page', async ({ page }) => {
+  
+  // Click nav link 
+  await page.getByRole('link', { name: 'Notebook', exact: true }).click();
+  
+  // Expect url to become the set page. Additional tests to follow.
+  await expect(page).toHaveURL('#notebook');
+
+});
+
+test('is world page', async ({ page }) => {
+  
+  // Click nav link 
+  await page.getByRole('link', { name: 'World', exact: true }).click();
+  
+  // Expect url to become the set page. Additional tests to follow.
+  await expect(page).toHaveURL('#world');
+
+});
+
+test('is lore page', async ({ page }) => {
+  
+  // Click nav link 
+  await page.getByRole('link', { name: 'Lore', exact: true }).click();
+  
+  // Expect url to become the set page. Additional tests to follow.
+  await expect(page).toHaveURL('#lore');
+
+});
+
+test('is homebrew page', async ({ page }) => {
+  
+  // Click nav link 
+  await page.getByRole('link', { name: 'Homebrew', exact: true }).click();
+  
+  // Expect url to become the set page. Additional tests to follow.
+  await expect(page).toHaveURL('#homebrew');
+
 });
