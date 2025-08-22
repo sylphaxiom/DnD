33eb28b2f5acfcc188ebdc0f97fae160ffe77975
Jacob Pell <pelljacoba@gmail.com>
2025-08-22 08:23:15 -0400

    small modifications to test deployment to a shared hosting environment

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+//import * as React from "react";
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
